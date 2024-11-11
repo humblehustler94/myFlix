1955f6cdf7f1816abfa913ab713f725a26728109
humblehustler94 <flores.itzel94@gmail.com>
2024-11-11 14:22:25 -0800

    Refactor index.js --2.4 Task
    refactor code for morgan.
    added static requests to server documentation.
    added error handling in express.

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // setting up express in project task 2.4
-// the code as of right opens now opens said files like instructed in 2.4 
-// /documentation, /movies = json, 
 const express = require('express');
+// new code 2.4
+const morgan = require('morgan');
+
 const app = express();
+
+// new code added 2.4
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+
+// Morgan middleware --> 2.4
+app.use(morgan('combined'));
 
 // Movie list 2.4
 // add 10 movies and year released.
@@ -62,35 +71,46 @@ let requestTime = (req, res, next) => {
     next();
 };
 
+
 app.use(myLogger);
 app.use(requestTime);
 
 // GET requests
 // simply retrieving data
-// refactored code.
-app.get('/',(req, res) => {
-    let responseText = 'Welcome to my app!';
-    responseText += '<small>Requested at:' + req.requestTime + '</small>';
-    res.send(responseText);
+app.get('/', (req, res) => {
+    res.send('Welcome to my app!');
 });
-/*
+
 app.get('/documentation', (req, res) => {
-    res.sendFile('public/documentation.html', {root: __dirname});
+    res.sendFile('public/documentation.html', { root: 'public' });
 });
-*/
+
 app.get('/secreturl', (req, res) => {
-    let responseText = 'This is a secret url with super top-secret content.';
-    responseText += '<small>Requested at:' + req.requestTime + '</small>';
-    res.send(responseText)
     res.send('This is a secret url with super top-secret content.');
 });
 
-app.get('/movies',(req, res) => {
+app.get('/movies', (req, res) => {
     res.json(topMovies);
 });
 
+// Static request for documentation --> 2.4 
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+// error handling in express --> 2.4
+app.use((err, req, res, next) => {
+    // logic
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 // listen for requests
-app.listen(8080,() => {
+app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
 });
