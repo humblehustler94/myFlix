13d3d1f1b682634b8619d4b1ace0bcc95222de50
humblehustler94 <flores.itzel94@gmail.com>
2024-12-18 11:24:10 -0800

    Refactor - Index.js for Mongoose
    Update code for mongoose.
    give comments on what the codes do.

--- index.js
+++ index.js
@@ -14,66 +14,11 @@ const Users = Models.User; // 2.8
 
 mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true }); // 2.8
 
-
 const app = express();
 // inserted after const app = express();
 // add these lines if using versions of express above 4.16 to import body-parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// comment out no longer needed.
-// Movie list 2.4
-// add 10 movies and year released.
-/*
-let topMovies = [
-
-    {
-        title: 'Instructions Not Included',
-        year: '2013'
-    },
-    {
-        title: 'Mighty Joe Young',
-        year: '1998'
-    },
-    {
-        title: 'The Iron Giant',
-        year: '1999'
-    },
-    {
-        title: 'Fast Five',
-        year: '2011'
-    },
-    {
-        title: 'The Breakfast Club',
-        year: '1985'
-    },
-    {
-        title: 'The Sandlot',
-        year: '1993'
-    },
-    {
-        title: 'Blade',
-        year: '1998'
-    },
-    {
-        title: 'Edward Scissorhands',
-        year: '1990'
-    },
-    {
-        title: 'Transformers',
-        year: '2007'
-    },
-    {
-        title: 'Grease',
-        year: '1978'
-    },
-
-];
-
-let users = [];
-
-let movies = [];
-*/
 
 // ADD middleware function: myLogger to project.
 let myLogger = (req, res, next) => {
@@ -86,26 +31,22 @@ let requestTime = (req, res, next) => {
     next();
 };
 
-// add this line after middleware
+let users = [];
+
+// CODE HERE FOR ---> app.use 
 app.use(myLogger);
 app.use(requestTime);
-
 app.use(bodyParser.json()); // 2.5
+app.use(morgan('common')); // APP USING MORGAN
 
-// APP USING MORGAN
-app.use(morgan('common'));
+// USING EXPRESS STATIC FOR DOCUMENTATION.HTML
+app.use(express.static('public'));
 
-// GET requests
-// DEFAULT
-//  http://localhost:8080/ --> Welcome to my app!
 
+// GET root requests in project --> 2.4
+// http://localhost:8080/ --> default
 app.get('/', (req, res) => {
     res.send('Welcome to my app!');
-});
-
-// might no longer need this code line below.
-app.get('/documentation', (req, res) => {
-    res.sendFile('public/documentation.html', { root: 'public' });
 });
 
 // http://localhost:8080/secreturl --> This is a secret url with super top-secret content. displayed in DOM.
@@ -113,11 +54,9 @@ app.get('/secreturl', (req, res) => {
     res.send('This is a secret url with super top-secret content.');
 });
 
-// READ IN MONGOOSE : CODE 2  --> 2.8
-// READ endpoint : get all movies
-// REFACTORED CODE
-// http://localhost:8080/movies --> results mongoDB movies prev task 2.7 
-// returns all 10 movies
+// READ IN MONGOOSE : Get all movies
+// READ endpoint : http://localhost:8080/movies
+// refactored code : return all movies from MongoDB 
 app.get('/movies', (req, res) => {
     Movies.find()
         .then((movies) => {
@@ -129,10 +68,8 @@ app.get('/movies', (req, res) => {
         });
 });
 
-// READ IN MONGOOSE : CODE 3 --> 2.8
-// READ endpoint : Get all users 
-// http://localhost:8080/users --> results mongoDB users prev task 2.7
-// returns all 4 users
+// READ IN MONGOOSE : Get all users 
+// READ endpoint : http://localhost:8080/users --> results mongoDB users from prev task 2.7
 app.get('/users', async (req, res) => {
     await Users.find()
         .then((users) => {
@@ -144,11 +81,9 @@ app.get('/users', async (req, res) => {
         })
 })
 
-// READ IN MONGOOSE : CODE 4 --> 2.8
-// READ endpoint : Get a user by username
-// http://localhost:8080/users/:Username --> results mongoDB users prev task 2.7
-// http://localhost:8080/users/johndoe 
-// returns 1 user "johndoe"
+// READ IN MONGOOSE : Get a user by username : CODE 4 --> 2.8
+// READ endpoint : http://localhost:8080/users/flores
+// returns 1 user "flores"
 app.get('/users/:Username', async (req, res) => {
     await Users.findOne({ Username: req.params.Username })
         .then((user) => {
@@ -160,19 +95,45 @@ app.get('/users/:Username', async (req, res) => {
         });
 });
 
-// 2.5 CRUD TO PROJECT video notes part 2 
-// CREATE -- new users
-//  ADD A USER 
-/* WE'LL EXCEPT JSON IN THIS FORMAT
-{
-ID: Integer,
-Username: String,
-Password: String,
-Email: String,
-Birthday: Date
-}
-*/
+// READ IN MONGOOSE : Get a movie by title
+// READ endpoint : http://localhost:8080/movies/Inception
+app.get('/movies/:Title', (req, res) => {
+    Movies.findOne({ Title: req.params.Title })
+        .then((movie) => {
+            res.json(movie);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error:" + err);
+        });
+});
 
+// READ IN MONGOOSE : return a genre by name 
+// Mongoose function for "finding" movie data for movies with a genre of "Thriller"
+// READ endpoint : http://localhost:8080/movies/genre/Thriller
+app.get('/movies/genre/:genreName', (req, res) => {
+    Movies.findOne({ "Genre.Name": req.params.genreName })
+        .then((movie) => res.json(movie.Genre))
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error:' + err);
+        });
+
+});
+
+// READ IN MONGOOSE : get a directors information ()
+// READ endpoint : http://localhost:8080/movies/directors/Christopher%20Nolan
+app.get('/movies/directors/:directorName', (req, res) => {
+    Movies.findOne({ "Director.Name": req.params.directorName })
+        .then((movie) => res.json(movie.Director))
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error:' + err);
+        });
+
+});
+
+// CREATE -- new users
 /*
 app.post('/users', (req, res) => {
     const newUser = req.body;
@@ -187,10 +148,8 @@ app.post('/users', (req, res) => {
 })
 */
 
-// CREATE IN MONGOOSE : CODE 1 --> 2.8
-// refactor the following code
-// CREATE endpoint : Add a new user
-// http://localhost:8080/users 
+// CREATE IN MONGOOSE : Add a new user : CODE 1 --> 2.8
+// CREATE endpoint : http://localhost:8080/users
 // body --> raw --> enter in Username, Password --> results in error asks for Email to Path --> ref MOCK 4 img.
 // add email results are MOCK 5 img.
 app.post('/users', async (req, res) => {
@@ -217,15 +176,10 @@ app.post('/users', async (req, res) => {
             console.error(error);
             res.status(500).send('Error:' + error);
         });
-    //res.send('Successful POST request creating a new user');
 });
 
-
-
-// UPDATE IN MONGOOSE : CODE 5 --> 2.8
-// UPDATE -- user 
-// http://localhost:8080/users/johndoe 
-// REFACTORED CODE : Update a user's info, by username
+// UPDATE IN MONGOOSE : Update a user by username / update a user's information : CODE 5 --> 2.8
+// UPDATE endpoint : http://localhost:8080/users/flores 
 /* we'll expect JSON in this format
 {
 Username: String,
@@ -257,6 +211,7 @@ app.put('/users/:Username', async (req, res) => {
 });
 
 
+// UPDATE -- user name
 app.put('/users/:id', (req, res) => {
     const { id } = req.params;
     const updatedUser = req.body;
@@ -272,8 +227,27 @@ app.put('/users/:id', (req, res) => {
 
 })
 
+
+// MONGOOSE - add a movie to a users favoriteMovies
+// POST endpoint - http://localhost:8080/users/flores/movies/674f6e99130c43c583893bfe
+app.post('/users/:Username/movies/:MovieID', async (req, res) => {
+    await Users.findOneAndUpdate(
+        { Username: req.params.Username },
+        { $set: { FavoriteMovies: req.params.MovieID } },
+        { new: true }
+    )
+        .then((updatedUser) => res.json(updatedUser))
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error:' + err);
+        });
+});
+
+
+// ------------------------------------ >   DO I DELETE THIS commnent out code? 
 // CREATE
 // adds a movie to favoriteMovies
+/*
 app.post('/users/:id/:movieTitle', (req, res) => {
     const { id, movieTitle } = req.params;
     const updatedUser = req.body;
@@ -288,25 +262,25 @@ app.post('/users/:id/:movieTitle', (req, res) => {
     }
 
 })
+*/
 
-// UPDATE IN MONGOOSE : CODE 6 --> 2.8
-// ADD A MOVIE TO A USER'S LIST OF FAVORITES
-app.post('users/:Usersname/movies/:MovieID', async (req, res) => {
-    await Users.findOneAndUpdate({ Username: req.params.Username }, {
-        $push: { FavoriteMovies: req.params.MovieID }
-    },
-        { new: true }) // THIS LINE MAKES SURE THAT THE UPDATED DOCUMENT IS RETURNED
-        .then((updatedUser) => {
-            res.json(updatedUser);
-        })
+
+// DELETE IN MONGOOSE : deletes a favorite movie from users favritesMovies list 
+// DELETE endpoint : http://localhost:8080/users/flores/movies/674f6e99130c43c583893bfe
+app.delete('/users/:Username/movies/:MovieID', async (req, res) => {
+    await Users.findOneAndUpdate(
+        { Username: req.params.Username },
+        { $pull: { FavoriteMovies: req.params.MovieID } },
+        { new: true }
+    )
+        .then((updatedUser) => res.json(updatedUser))
         .catch((err) => {
             console.error(err);
             res.status(500).send('Error:' + err);
         });
 });
 
-// DELETE
-// deletes a favorite movie from users favoritesMovie list
+/*
 app.delete('/users/:id/:movieTitle', (req, res) => {
     const { id, movieTitle } = req.params;
     const updatedUser = req.body;
@@ -321,8 +295,10 @@ app.delete('/users/:id/:movieTitle', (req, res) => {
     }
 
 })
+*/
 
 
+/*
 // DELETE -- remove a user
 app.delete('/users/:id', (req, res) => {
     const { id } = req.params;
@@ -339,17 +315,18 @@ app.delete('/users/:id', (req, res) => {
     }
 
 })
+*/
 
-// DELETE IN MONGOOSE : CODE 5  --> 2.8 
-// Delete a user by username
-// http://localhost:8080/user/:Username
+
+// DELETE IN MONGOOSE : Delete a user by username
+// DELETE endpoint : http://localhost:8080/user/flores
 app.delete('/users/:Username', async (req, res) => {
-    await Users.findOneAndRemove({ Username: req.params.Username })
+    await Users.findOneAndDelete({ Username: req.params.Username })
         .then((user) => {
             if (!user) {
-                res.status(400).send(req.params.Username + 'was not found');
+                res.status(400).send(req.params.Username + ' was not found');
             } else {
-                res.status(200).send(req.params.Username + 'was deleted.');
+                res.status(200).send(req.params.Username + ' was deleted.');
             }
         })
         .catch((err) => {
@@ -358,57 +335,10 @@ app.delete('/users/:Username', async (req, res) => {
         });
 });
 
-// 2.5 CRUD TO PROJECT video notes part 1 
-// make a READ endpoint
-// returns a movie title
-app.get('/movies/:title', (req, res) => {
-    //const title = req.params.title; // old way
-    const { title } = req.params; // creating a new variable title // deconstruced code.
-    const movie = movie.find(movie => movie.Tilte === title);
-
-    if (movie) {
-        res.status(200).json(movies);
-    } else {
-        res.status(400).send('no such movie')
-    }
-
-})
-
-// make a READ endpoint
-// return a genre by name
-app.get('/movies/genre/:genreName', (req, res) => {
-    //const title = req.params.title; // old way
-    const { genreName } = req.params; // creating a new variable title // deconstruced code.
-    const genre = movies.find(movie => movie.Genre.Name === genreName).Genre; // .get method
-
-    if (genre) {
-        res.status(200).json(genre);
-    } else {
-        res.status(400).send('no such genre')
-    }
-
-})
-
-// make a READ endpoint
-// return a directors name
-app.get('/movies/directors/:directorName', (req, res) => {
-    //const title = req.params.title; // old way
-    const { directorName } = req.params; // creating a new variable title // deconstruced code.
-    const director = movies.find(movie => movie.Director.Name === directorName).Director; // .get method
-
-    if (director) {
-        res.status(200).json(director);
-    } else {
-        res.status(400).send('no such director')
-    }
-
-})
-
-// USING EXPRESS STATIC FOR DOCUMENTATION.HTML
-app.use(express.static('public'));
 
 
 // ADD MIDDLEWEAR FUNCTION : ERROR-HANDLING 
+// error handling in express --> 2.4
 app.use((err, req, res, next) => {
     // logic
     console.error(err.stack);
@@ -416,7 +346,7 @@ app.use((err, req, res, next) => {
 });
 
 
-// LISTEN FOR REQUESTS ON PORT 
+// listen for requests
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
 });
