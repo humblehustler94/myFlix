d870b491e84a71fdfffab6ec0dabea01643ad906
humblehustler94 <flores.itzel94@gmail.com>
2025-01-02 16:56:10 -0800

    Update index.js
    refactor code lines 220 - 225
    these lines were added in section 2.9 of task.

--- index.js
+++ index.js
@@ -216,7 +216,13 @@ Email: String,
 (required)
 Birthday: Date
 }*/
-app.put('/users/:Username', async (req, res) => {
+// refactor code 2.9 
+app.put('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    // condition to check added here
+    if (req.user.Username !== req.params.Username) {
+        return res.status(400).send('Permission denied');
+    }
+    // condition ends 
     await Users.findOneAndUpdate({ Username: req.params.Username }, {
         $set:
         {
