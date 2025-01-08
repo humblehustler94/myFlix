48da0bb528e135d163603ba05d56825d0fa1d857
humblehustler94 <flores.itzel94@gmail.com>
2025-01-08 12:28:20 -0800

    Update model.js file
    new code added to import the module into models.js file.
    code line : 5
    
    Add 2 new function to user schema code:
    hashPassword function : code line 30-33
    validatePassword function : code lines 35-38

--- models.js
+++ models.js
@@ -1,9 +1,12 @@
 /* 2.8 config mongoose into project */
 const mongoose = require('mongoose');
 
+// new code added to import the module into models.js 
+const bcrypt = require('bcrypt');
+
 let movieSchema = mongoose.Schema({
-    Title: {type: String, required: true},
-    Description: {type: String, required: true},
+    Title: { type: String, required: true },
+    Description: { type: String, required: true },
     Genre: {
         Name: String,
         Description: String
@@ -17,12 +20,23 @@ let movieSchema = mongoose.Schema({
 });
 
 let userSchema = mongoose.Schema({
-    Username: {type: String, required: true},
-    Password: {type: String, required: true},
-    Email: {type: String, required:  true},
+    Username: { type: String, required: true },
+    Password: { type: String, required: true },
+    Email: { type: String, required: true },
     Birthday: Date,
-    FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+
+// add hashPassword function
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+};
+
+// add validatePassword function
+userSchema.methods.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.Password);
+};
+
 
 // this will create collections called db.movies and db.users
 // 'Movie' --> db.movies
