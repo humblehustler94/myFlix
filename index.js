f5df24173411bfa6f32272078f3f4ab24b61d912
humblehustler94 <flores.itzel94@gmail.com>
2025-01-08 11:49:54 -0800

    Update index.js to include CORS
    The above code creates a list of allowed domains within variable allowedOrigins.
    Then compares the domains of any incoming request with this list and either allows it (if the domain is on the list) or returns an error ( if the domain isn't on the list)
    
    code lines 24-39 --> added in 2.10

--- index.js
+++ index.js
@@ -21,6 +21,22 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+// 2.10 how use CORS within your application
+const cors = require('cors');
+
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+/* rest of code goes here*/
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) { // if a specific origin isn't found on the list of allowed origins
+            let message = 'The CORS policy for this application does not allow access from origin' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 
 let auth = require('./auth')(app); // import auth.js file into project 2.9
 // add the following code after 
