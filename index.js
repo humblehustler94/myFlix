a6eb323957c1f7f5b8abc4771673eab04bdd18bf
humblehustler94 <flores.itzel94@gmail.com>
2025-01-31 12:21:14 -0800

    Update index.js
    removed quation marks from process.env.CONNECTION_URI

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const Users = Models.User; // 2.8
 
 // this mongoose.connect allows you to test in postman before changing it in 2.10 for heroku app.
 //mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true }); // 2.8
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true }); // 2.8
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true }); // 2.8
 
 
 
