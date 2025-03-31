ba28edefc3c44f4c1df4adf4267e56537eed5608
humblehustler94 <flores.itzel94@gmail.com>
2025-03-31 10:11:03 -0700

    Update index.js
    Re-enable token-based authentication on /movies endpoint

--- index.js
+++ index.js
@@ -108,7 +108,7 @@ app.get('/movies', (req, res) => {
 
 // applying JWT  authentication to a specific endpoint 
 // refactor the following code 2.9
-app.get('/movies', /*passport.authenticate('jwt', { session: false })*/ async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
     await Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
